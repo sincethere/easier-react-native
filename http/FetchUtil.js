dcf9d8dd62adf136e2f2678afbcbf8cc9aac3219
wenxucheng <wenxucheng@ltbl.cn>
2016-03-16 10:36:50 +0800

    http请求封装

--- http/FetchUtil.js
+++ http/FetchUtil.js
@@ -22,9 +22,6 @@ class FetchUtil {
 		this.credentials   = 'omit';
 		this.return_type   = 'json';
 		this.time          = 0;
-		this.timer         = null;
-		this.timer_done    = false;
-		this.timeout_event = ()=>{console.log('the http request timeout')};
 
 		return this;
 	}
@@ -52,11 +49,6 @@ class FetchUtil {
 
 	setTime(val){
 		this.time = val;
-		return this;
-	}
-
-	setTimeoutEvent(val){
-		this.timeout_event = val;
 		return this;
 	}
 
@@ -93,11 +85,6 @@ class FetchUtil {
 		return this;
 	}
 
-	timeout(){
-		this.timer_done = true;
-		this.timeout_event();
-	}
-
 	dofetch(){
 		let options         = {};
 		options.method      = this.method;
@@ -107,20 +94,18 @@ class FetchUtil {
 			options.headers = this.headers;
 		}
 
-		this.url = _formatUrl(this.url, this.bodys);
-
 		if({} != this.bodys && this.method != 'GET'){
 			if('form' == this.body_type){
 				this.setHeader("Content-Type","application/x-www-form-urlencoded;charset=UTF-8");
 				let data = '';
-				Object.keys(this.bodys).map((index)=>{
+				Object.keys(this.bodys).map((index) => {
 					let param = encodeURI(this.bodys[index]);
 					data += `${index}=${param}&`;
 				});
 				options.body = data;
 			}else if('file' == this.body_type){
 				let data = new FormData();
-				Object.keys(this.bodys).map((index)=>{
+				Object.keys(this.bodys).map((index) => {
 					data.append(index, this.bodys[index]);
 				});
 				options.body = data;
@@ -129,67 +114,14 @@ class FetchUtil {
 			}
 		}
 
-		if(this.time){
-			this.timer = setTimeout(()=>{
-				this.timeout();
-			},this.time);
-		}
-
-		return fetch(this.url,options).then(
-			(response)=>{
-				this.clearTimeoutCheck();
-				this.checkStatus(response);
-				return response;
-			}
-		).then(
-			(response)=>{
-				if('json' == this.return_type){
-					return response.json();
-				}else if('text' == this.return_type){
-					return response.text();
-				}else if('blob' == this.return_type){
-					return response.blob();
-				}else if('formData' == this.return_type){
-					return response.formData();
-				}else if('arrayBuffer' == this.return_type){
-					return response.arrayBuffer();
-				}
-			}
-		).catch(
-			(err)=>{
-				this.clearTimeoutCheck(err);
-				throw err;
-			}
-		);
+		return new Promise([
+			fetch(this.url,options),
+			new Promise(function (resolve, reject) {
+			    setTimeout(() => reject(new Error('request timeout')), this.time)
+			})
+		]);
 	}
 
-	checkStatus(response){
-		console.log(response);
-		//throw new Error('no login');
-	}
-
-	clearTimeoutCheck(err = null){
-		if(this.timer != null){
-			clearTimeout(this.timer);
-		}
-		//防止正常超时返回的数据
-		if(this.timer_done == true){
-			throw new Error('timeout');
-		}
-	}
-
-	//如果api中带有{}携带参数格式化
-    _formatUrl(url, params) {
-        if (url.includes('{') && !!params) {
-        	for (let i = params.keys().length-1; i >= 0 ; i--) {
-				let name = params.keys()[i];
-        		if (url.includes(name)) {
-        			url = url.replace('{'+name+'}', params[name]);
-        		}
-        	}
-        }
-		return url;
-    }
 }
 
 module.exports = FetchUtil;
--- http/HttpRequest.js
+++ http/HttpRequest.js
@@ -1,30 +1,69 @@
+/**
+ * Created by wenxucheng on 16/2/29.
+    此类为根据平台特定协议扩展FetchUtil
+ */
+
 'use strict';
 
-module.exports = {
-    /**
-     * options {url:'http://www.baidu.com', type:'POST', headers:{}, data:{}, dataType:'json', success: function(data, headers){}, error: function(status, data, headers){}, networkError: function() {}}
-     */
-    request(options) => {
-        var fetchOptions = {
-            method: options.method,
-            headers: options.headers,
-        };
-        if(options.method != 'GET'){
-            fetchOptions.body = options.body;
-        }
-        fetch(options.url, fetchOptions)
-            .then((response) => {
-                if (response.status == 200) {
-                    response.json().then((data) => {
-                        options.success(data, response.headers);
-                    });
-                } else {
-                    options.error(response.status);
-                }
-            })
-            .catch((err) => {
-                options.networkError(err);
-            })
-            .done();
+import FetchUtil from './FetchUtil';
+
+class HttpRequest extends FetchUtil {
+
+	constructor(url) {
+        super(url);
     }
-};
+
+	dofetch() {
+        this.url = _formatUrl(this.url, this.bodys);
+
+		return super.dofetch()
+            .then(
+    			(response) => {
+    				this.checkStatus(response);
+    				return response;
+    			}
+    		)
+    		.then(
+    			(response) => {
+    				if('json' == this.return_type){
+    					return response.json();
+    				}else if('text' == this.return_type){
+    					return response.text();
+    				}else if('blob' == this.return_type){
+    					return response.blob();
+    				}else if('formData' == this.return_type){
+    					return response.formData();
+    				}else if('arrayBuffer' == this.return_type){
+    					return response.arrayBuffer();
+    				}
+    			}
+    		)
+    		.catch(
+    			(err) => {
+    				throw err;
+    			}
+    		);
+	}
+
+
+	checkStatus(response){
+		console.log(response);
+		//throw new Error('no login');
+	}
+
+	//如果api中带有{}携带参数格式化
+    _formatUrl(url, params) {
+        if (url.includes('{') && !!params) {
+        	for (let i = params.keys().length-1; i >= 0 ; i--) {
+				let name = params.keys()[i];
+        		if (url.includes(name)) {
+        			url = url.replace('{'+name+'}', params[name]);
+        		}
+        	}
+        }
+		return url;
+    }
+
+}
+
+module.exports = HttpRequest;
